fcb75773afd17a1f8094b7c5a97e7eb569f65a4f
Pablo <>
2025-02-04 23:37:48 +0100

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import esLintConfigPrettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default tseslint.config(
+    esLintConfigPrettier,
+    { ignores: ['dist'] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'prettier': prettierPlugin,
+        },
+        rules: {
+            ...esLintConfigPrettier.rules,
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            semi: ['error', 'never'],
+            quotes: ['error', 'single'],
+            'no-multiple-empty-lines': ['error', { max: 1 }],
+            'prettier/prettier': [
+                'error',
+                {
+                    singleQuote: true,
+                    parser: 'typescript',
+                    semi: false,
+                },
+            ],
+        },
+    },
+)
+
+
+
+
+
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+
+function App() {
+  return <div className={'p-5 bg-blue-500 text-white'}>ciao</div>
+}
+
+export default App
